cc6c104507b21bfb42f11f8b665ca3658ac19ee6
Francois Seguin <seguin.mtl@gmail.com>
2022-10-21 04:16:14 +0000

    uipdating hasura command

--- src/lib/command-hasura.mjs
+++ src/lib/command-hasura.mjs
@@ -45,6 +45,75 @@ export async function hasuraOpenConsole() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// SQUASH MIGRATIONS
+////////////////////////////////////////////////////////////////////////////////
+
+export async function hasuraMigrateSquash(version) {
+  const { hasura: hasuraConfig } = metaConfig;
+
+  const { state } = { ...hasuraConfigDefault, ...hasuraConfig };
+
+  cd(`${currentPath}/${state}`);
+
+  $.verbose = true;
+  await $`hasura migrate squash --from ${version} --database-name default`;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CREATE MIGRATIONS
+////////////////////////////////////////////////////////////////////////////////
+
+export async function hasuraMigrateCreate(name) {
+  const { hasura: hasuraConfig } = metaConfig;
+
+  const { state } = { ...hasuraConfigDefault, ...hasuraConfig };
+
+  cd(`${currentPath}/${state}`);
+
+  $.verbose = true;
+
+  await $`hasura migrate create "${name}" --from-server --database-name default`;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// APPLY MIGRATIONS
+////////////////////////////////////////////////////////////////////////////////
+
+export async function hasuraMigrateApply(version) {
+  const { hasura: hasuraConfig } = metaConfig;
+
+  const { state } = { ...hasuraConfigDefault, ...hasuraConfig };
+
+  cd(`${currentPath}/${state}`);
+
+  $.verbose = true;
+  await $`hasura migrate apply --version ${version} --skip-execution --database-name default`;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//  HASURA GLOBAL
+////////////////////////////////////////////////////////////////////////////////
+
+export async function hasuraGlobalCmd(commands, options) {
+  const { hasura: hasuraConfig } = metaConfig;
+
+  const { databaseName } = options;
+
+  const { state } = { ...hasuraConfigDefault, ...hasuraConfig };
+
+  cd(`${currentPath}/${state}`);
+
+  $.verbose = true;
+
+  if (databaseName !== undefined) {
+    commands.push(`--database-name`);
+    commands.push(`${databaseName}`);
+  }
+
+  await $`hasura ${commands}`;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // MAIN ENTRY POINT
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -52,9 +121,34 @@ export default async function hasura(program) {
   const hasura = program.command('hasura');
   hasura.description('perform hasura maintenances');
 
-  const hasruaConsole = hasura.command('console');
+  const hasuraConsole = hasura.command('console');
+  const hasuraMigrate = hasura.command('migrate');
 
-  hasruaConsole
+  hasuraConsole
     .description('open hasura console locally ')
     .action(hasuraOpenConsole);
+
+  const migrateSquash = hasuraMigrate.command('squash');
+  migrateSquash
+    .description('squash all migrations')
+    .argument('<version>', 'version to squash to')
+    .action(hasuraMigrateSquash);
+
+  const migrateApply = hasuraMigrate.command('apply');
+  migrateApply
+    .description('apply all migrations')
+    .argument('<version>', 'version to apply')
+    .action(hasuraMigrateApply);
+
+  const migrateCreate = hasuraMigrate.command('create');
+  migrateCreate
+    .description('create a new migration from current schema')
+    .argument('<name>', 'name of the migration')
+    .action(hasuraMigrateCreate);
+
+  const hasuraCommand = hasura.command('cmd');
+  hasuraCommand
+    .argument('<commands...>', 'command to run')
+    .option('--database-name <database-name>', 'database name')
+    .action(hasuraGlobalCmd);
 }
