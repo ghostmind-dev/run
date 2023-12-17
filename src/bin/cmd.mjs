b3de5b9b241638cec3b6a27c5593e664efa355c1
François Séguin <seguin.mtl@gmail.com>
2023-12-17 04:41:07 +0000

    adding npm command

--- src/bin/cmd.mjs
+++ src/bin/cmd.mjs
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
-import { $ } from "zx";
-import { config } from "dotenv";
-import _ from "lodash";
-import { Command, Option } from "commander";
-import commandTerraform from "../lib/command-terraform.mjs";
-import commandCustom from "../lib/command-custom.mjs";
-import commandVault from "../lib/command-vault.mjs";
-import commandAction from "../lib/command-action.mjs";
-import commandGithub from "../lib/command-github.mjs";
-import commandSkaffold from "../lib/command-skaffold.mjs";
-import commandHasura from "../lib/command-hasura.mjs";
-import commandCluster from "../lib/command-cluster.mjs";
-import commandDb from "../lib/command-db.mjs";
-import commandUtils from "../lib/command-utils.mjs";
-import commandDocker from "../lib/command-docker.mjs";
-import commandVercel from "../lib/command-vercel.mjs";
-import commandLib from "../lib/command-lib.mjs";
-import commandMachine from "../lib/command-machine.mjs";
-import commandGhost from "../lib/command-ghost.mjs";
+import { $ } from 'zx';
+import { config } from 'dotenv';
+import _ from 'lodash';
+import { Command, Option } from 'commander';
+import commandTerraform from '../lib/command-terraform.mjs';
+import commandCustom from '../lib/command-custom.mjs';
+import commandVault from '../lib/command-vault.mjs';
+import commandAction from '../lib/command-action.mjs';
+import commandGithub from '../lib/command-github.mjs';
+import commandSkaffold from '../lib/command-skaffold.mjs';
+import commandHasura from '../lib/command-hasura.mjs';
+import commandCluster from '../lib/command-cluster.mjs';
+import commandDb from '../lib/command-db.mjs';
+import commandUtils from '../lib/command-utils.mjs';
+import commandDocker from '../lib/command-docker.mjs';
+import commandVercel from '../lib/command-vercel.mjs';
+import commandLib from '../lib/command-lib.mjs';
+import commandMachine from '../lib/command-machine.mjs';
+import commandGhost from '../lib/command-ghost.mjs';
+import commandNpm from '../lib/command-npm.mjs';
 
 ////////////////////////////////////////////////////////////////////////////////
 // CONST
@@ -41,7 +42,7 @@ $.verbose = false;
 const program = new Command();
 
 program.addOption(
-  new Option("--env-filename <filename>", "env filename to load")
+  new Option('--env-filename <filename>', 'env filename to load')
 );
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -57,9 +58,9 @@ let paths = [];
 if (SRC !== undefined) {
   while (initPath !== SRC) {
     paths.push(initPath);
-    const pathParts = initPath.split("/");
+    const pathParts = initPath.split('/');
     pathParts.pop(); // Remove the last element
-    initPath = pathParts.join("/");
+    initPath = pathParts.join('/');
   }
 
   -_.reverse(paths).map((path) => config({ path: `${path}/.env` }));
@@ -73,7 +74,7 @@ if (SRC !== undefined) {
 
 program.exitOverride();
 
-program.name("run");
+program.name('run');
 
 ////////////////////////////////////////////////////////////////////////////////
 // GIT COMMAND
@@ -94,6 +95,7 @@ await commandDocker(program);
 await commandVercel(program);
 await commandLib(program);
 await commandGhost(program);
+await commandNpm(program);
 
 ////////////////////////////////////////////////////////////////////////////////
 // PARSING ARGUMENTS
@@ -104,8 +106,8 @@ try {
 } catch (err) {
   const { exitCode, name, code, message } = err;
 
-  if (!message.includes("outputHelp")) {
-    console.error("something went wrong");
+  if (!message.includes('outputHelp')) {
+    console.error('something went wrong');
   }
 }
 
