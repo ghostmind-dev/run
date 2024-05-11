59c330d16e108305c475bd62bc70e5627ed20e1b
Francois Seguin <seguin.mtl@gmail.com>
2024-05-11 01:45:21 +0000

    adding utils/divers.ts types

--- run/types/global.d.ts
+++ run/types/global.d.ts
@@ -82,6 +82,95 @@ interface ModuleActions {
   vaultKvVaultToLocalAll(): Promise<void>;
   vaultKvVaultToLocalEntry(): Promise<void>;
   vaultKvVaultToLocalUnit(): Promise<void>;
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // UTILS?DIVERS
+  ////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Retrieves the application name.
+   */
+  getAppName(): Promise<string>;
+
+  /**
+   * Retrieves the project name.
+   */
+  getProjectName(): Promise<string>;
+
+  /**
+   * Sets the current environment locally.
+   */
+  setEnvOnLocal(): Promise<void>;
+
+  /**
+   * Sets secrets for the target environment locally.
+   * @param target - The target environment identifier.
+   */
+  setSecretsOnLocal(target: string): Promise<void>;
+
+  /**
+   * Detects the directory of scripts.
+   * @param currentPath - The path to start detection.
+   * @returns The detected directory path.
+   */
+  detectScriptsDirectory(currentPath: string): Promise<string>;
+
+  /**
+   * Verifies if the project core exists.
+   * @returns True if the project core is detected; false otherwise.
+   */
+  verifyIfProjectCore(): Promise<boolean>;
+
+  /**
+   * Gets files in a specified directory.
+   * @param path - The directory path.
+   * @returns An array of filenames.
+   */
+  getFilesInDirectory(path: string): Promise<string[]>;
+
+  /**
+   * Returns all directory names in a specified path.
+   * @param path - The directory path.
+   * @returns An array of directory names.
+   */
+  getDirectories(path: string): Promise<string[]>;
+
+  /**
+   * Recursively discovers directories in a given path.
+   * @param path - The starting directory path.
+   * @returns An array of paths to directories.
+   */
+  recursiveDirectoriesDiscovery(path: string): Promise<string[]>;
+
+  /**
+   * Verifies if a `meta.json` file exists in the current directory.
+   * @param path - The directory path.
+   * @returns The parsed `meta.json` content or `false` if not found.
+   */
+  verifyIfMetaJsonExists(path: string): Promise<any>;
+
+  /**
+   * Matches directories based on the specified `meta.json` property condition.
+   * @param property - The property to match.
+   * @param value - The value to match (optional).
+   * @param path - The root directory to start searching.
+   * @returns An array of paths matching the specified condition.
+   */
+  withMetaMatching({
+    property,
+    value,
+    path,
+  }: {
+    property: string;
+    value?: string;
+    path?: string;
+  }): Promise<any[]>;
+
+  /**
+   * Sets secrets up to the specified project directory.
+   * @param path - The starting directory path.
+   */
+  setSecretsUptoProject(path: string): Promise<void>;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
