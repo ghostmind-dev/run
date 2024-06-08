4b8ecb06adada6b21e39f2ff954bbaf31d9c121b
Francois Seguin <seguin.mtl@gmail.com>
2024-06-08 15:25:04 -0400

    routines

--- run/lib/routine.ts
+++ run/lib/routine.ts
@@ -1,4 +1,4 @@
-import { $, cd } from 'npm:zx@8.1.0';
+import { $, cd, within } from 'npm:zx@8.1.0';
 import {
   detectScriptsDirectory,
   verifyIfMetaJsonExists,
@@ -29,19 +29,25 @@ let metaConfig = await verifyIfMetaJsonExists(currentPath);
 ////////////////////////////////////////////////////////////////////////////////
 //
 ////////////////////////////////////////////////////////////////////////////////
-
 export async function generateTreeCommands(
-  routines: string[],
+  scripts: string[],
   routineMap: any
 ): Promise<any> {
-  function splitCommands(command, routines) {
+  function resolveRoutine(task: string, routines: any): any {
+    const command = routines[task];
+
+    if (!command) {
+      return task; // If it's not a routine, return the task itself
+    }
+
+    // If the command starts with 'parallel ' or 'sequence ', handle them specially
     if (command.startsWith('parallel ')) {
       const parallelTasks = command
         .slice(9)
         .split(' ')
         .map((task) => task.trim());
       return {
-        tasks: parallelTasks.flatMap((task) => splitCommands(task, routines)),
+        tasks: parallelTasks.map((task) => resolveRoutine(task, routines)),
         mode: 'parallel',
       };
     }
@@ -52,55 +58,48 @@ export async function generateTreeCommands(
         .split(' ')
         .map((task) => task.trim());
       return {
-        tasks: sequenceTasks.flatMap((task) => splitCommands(task, routines)),
+        tasks: sequenceTasks.map((task) => resolveRoutine(task, routines)),
         mode: 'sequence',
       };
     }
 
-    // Split the command string by && for sequence and & for parallel if it contains such commands
+    // Split the command string by && for sequence
     const sequenceParts = command.split('&&').map((part) => part.trim());
     if (sequenceParts.length > 1) {
       return {
-        tasks: sequenceParts.flatMap((seqPart) =>
-          splitCommands(seqPart, routines)
-        ),
+        tasks: sequenceParts.map((part) => resolveRoutine(part, routines)),
         mode: 'sequence',
       };
     }
 
+    // Split the command string by & for parallel
     const parallelParts = command.split('&').map((part) => part.trim());
     if (parallelParts.length > 1) {
       return {
-        tasks: parallelParts.flatMap((parPart) =>
-          splitCommands(parPart, routines)
-        ),
+        tasks: parallelParts.map((part) => resolveRoutine(part, routines)),
         mode: 'parallel',
       };
     }
 
-    if (routines[command]) {
-      return splitCommands(routines[command], routines);
-    }
-
+    // If the command does not contain any of the above patterns, return the command itself
     return command;
   }
 
-  function buildTaskTree(tasks, routines) {
+  function buildTaskTree(tasks: string[], routines: any) {
     return tasks.map((task) => {
       if (routines[task]) {
-        const splitTask = splitCommands(routines[task], routines);
-        if (typeof splitTask === 'string') {
-          return splitTask;
-        } else {
-          return splitTask;
-        }
+        return resolveRoutine(task, routines);
       } else {
         return task;
       }
     });
   }
 
-  function generateObjectTree(tasks, routines, initialMode = 'parallel') {
+  function generateObjectTree(
+    tasks: string[],
+    routines: any,
+    initialMode = 'parallel'
+  ) {
     const taskTree = buildTaskTree(tasks, routines);
     return {
       tasks: taskTree,
@@ -108,7 +107,7 @@ export async function generateTreeCommands(
     };
   }
 
-  return generateObjectTree(routines, routineMap);
+  return generateObjectTree(scripts, routineMap);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -123,10 +122,12 @@ export default async function npm(program: any) {
     .argument('<script...>', 'script to run')
     .option('--parallel', 'Run scripts in parallel')
     .option('--sequence', 'Run scripts in sequence')
-    .action(async (scripts: string | string[], options: any) => {
+    .action(async (scripts: string[], options: any) => {
       $.verbose = false;
 
-      const routines = metaConfig?.routines as RoutineMap;
+      Deno.env.set('FORCE_COLOR', '1');
+
+      const routines = metaConfig?.routines;
 
       if (!routines) {
         console.log('No routines found');
@@ -135,26 +136,31 @@ export default async function npm(program: any) {
 
       const result = await generateTreeCommands(scripts, routines);
 
-      /**
-       * Execute a single command using google/zx.
-       * @param {string} command - The command to execute.
-       */
       async function executeCommand(command) {
         $.verbose = true;
-        const isCustomCommand = cmd`${command}`;
-        await $`${isCustomCommand}`;
+
+        // if command start with cd then change directory
+
+        if (command.startsWith('cd ')) {
+          const directory = command.slice(3);
+          await cd(directory);
+          return;
+        } else {
+          const isCustomCommand = cmd`${command}`;
+          await $`${isCustomCommand}`;
+        }
       }
 
       /**
        * Recursively execute tasks based on their mode.
        * @param {object} taskObject - The task object containing tasks and mode.
        */
-      async function executeTasks(taskObject) {
+      async function executeTasks(taskObject: any) {
         const { tasks, mode } = taskObject;
 
         if (mode === 'parallel') {
           await Promise.all(
-            tasks.map((task) => {
+            tasks.map((task: any) => {
               if (typeof task === 'string') {
                 return executeCommand(task);
               } else {
@@ -163,13 +169,15 @@ export default async function npm(program: any) {
             })
           );
         } else if (mode === 'sequence') {
-          for (const task of tasks) {
-            if (typeof task === 'string') {
-              await executeCommand(task);
-            } else {
-              await executeTasks(task);
+          await within(async () => {
+            for await (const task of tasks) {
+              if (typeof task === 'string') {
+                await executeCommand(task);
+              } else {
+                await executeTasks(task);
+              }
             }
-          }
+          });
         }
       }
 
