60356fcb8f69aee5d742afd8eee6de50b7bf000b
Francois Seguin <seguin.mtl@gmail.com>
2024-10-26 00:37:06 +0000

    updating tunnel

--- run/lib/tunnel.ts
+++ run/lib/tunnel.ts
@@ -6,6 +6,7 @@ import {
   setSecretsOnLocal,
 } from '../utils/divers.ts';
 import yaml from 'npm:js-yaml@4.1.0';
+import meta from './meta.ts';
 
 ///////////////////////////////////////////////////////////////////////////////
 // MUTE BY DEFAULT
@@ -31,90 +32,66 @@ export default async function tunnel(program: any) {
 
   const run = tunnel.command('run');
   run.description('Run a cloudflared tunnel to a local service');
+  run.argument('[tunnel]', 'tunnel to run', 'default');
   run.option('--all', 'Run all the services');
-  run.option('--tunnel <tunnel>', 'Set the tunnel name');
-  run.action(async (options: any) => {
-    const CLOUDFLARED_TUNNEL_TOKEN = Deno.env.get('CLOUDFLARED_TUNNEL_TOKEN');
-    const CLOUDFLARED_TUNNEL_NAME =
-      options.tunnel || Deno.env.get('CLOUDFLARED_TUNNEL_NAME');
-
-    const CLOUDFLARED_TUNNEL_URL = Deno.env.get('CLOUDFLARED_TUNNEL_URL') || '';
+  run.option(
+    '--name <name>',
+    'tunel name (or set via CLOUDFLARED_TUNNEL_NAME) ',
+    Deno.env.get('CLOUDFLARED_TUNNEL_NAME') as string
+  );
+  run.action(async (tunnelToRun: string, options: any) => {
+    const CLOUDFLARED_TUNNEL_NAME = options.name;
 
     interface Ingress {
       // add hostname (optional)
       hostname?: string;
       service: string;
     }
-
     interface Tunnel {
       tunnel: string;
       ingress: Ingress[];
     }
-
     let config: Tunnel = {
       tunnel: CLOUDFLARED_TUNNEL_NAME,
       ingress: [],
     };
-
     $.verbose = true;
-
     let ingress = [];
-
     if (options.all) {
       const directories = await withMetaMatching({ property: 'tunnel' });
-
       for (const directory of directories) {
         cd(directory);
         await setSecretsOnLocal('local');
         const metaConfig: any = await verifyIfMetaJsonExists(directory);
 
-        const subdomain = metaConfig.tunnel.subdomain;
-
-        let hostname = '';
-        if (subdomain) {
-          hostname = `${subdomain}.${CLOUDFLARED_TUNNEL_URL}`;
-        } else {
-          hostname = `${CLOUDFLARED_TUNNEL_URL}`;
+        for (const tunnel in metaConfig.tunnel) {
+          await $`cloudflared tunnel route dns ${CLOUDFLARED_TUNNEL_NAME} ${metaConfig.tunnel[tunnel].hostname}`;
+          ingress.push({
+            hostname: metaConfig.tunnel[tunnel].hostname,
+            service: metaConfig.tunnel[tunnel].service,
+          });
         }
-        await $`cloudflared tunnel route dns ${CLOUDFLARED_TUNNEL_NAME} ${hostname}`;
-
-        ingress.push({
-          hostname,
-          service: metaConfig.tunnel.service,
-        });
       }
 
       config.ingress = ingress;
     } else {
       let { tunnel }: any = await verifyIfMetaJsonExists(currentPath);
 
-      let hostname = '';
-
-      let subdomain = tunnel.subdomain;
-
-      if (subdomain) {
-        hostname = `${subdomain}.${CLOUDFLARED_TUNNEL_URL}`;
-      } else {
-        hostname = `${CLOUDFLARED_TUNNEL_URL}`;
-      }
-
       $.verbose = true;
-
-      await $`cloudflared tunnel route dns ${CLOUDFLARED_TUNNEL_NAME} ${hostname}`;
-      ingress.push({ hostname, service: tunnel.service });
+      await $`cloudflared tunnel route dns ${CLOUDFLARED_TUNNEL_NAME} ${tunnel[tunnelToRun].hostname}`;
+      ingress.push({
+        hostname: tunnel[tunnelToRun].hostname,
+        service: tunnel[tunnelToRun].service,
+      });
       config.ingress = ingress;
     }
-
     config.ingress.push({ service: 'http_status:404' });
-
     console.log(config);
-
     await $`rm -f /home/vscode/.cloudflared/config.yaml`;
-
     const yamlStr = yaml.dump(config);
 
+    const CLOUDFLARED_TUNNEL_TOKEN = Deno.env.get('CLOUDFLARED_TUNNEL_TOKEN');
     await fs.writeFile('/home/vscode/.cloudflared/config.yaml', yamlStr);
-
     await $`cloudflared tunnel --config /home/vscode/.cloudflared/config.yaml --protocol http2 run --token ${CLOUDFLARED_TUNNEL_TOKEN} ${CLOUDFLARED_TUNNEL_NAME}`;
   });
 }
