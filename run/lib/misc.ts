b2995335d86c920d447a99367d4b8f78c6e5753a
Francois Seguin <seguin.mtl@gmail.com>
2024-11-25 03:57:55 +0000

    removing misc exec

--- run/lib/misc.ts
+++ run/lib/misc.ts
@@ -112,35 +112,6 @@ export default async function misc(program: any) {
   ////////////////////////////////////////////////////////////////////////////
 
   misc
-    .command('exec')
-    .description('initiate an interactive shell in a running devcontainer')
-
-    .argument('[name]', 'name of the devcontainer')
-    .action(async (container: string) => {
-      try {
-        $.verbose = true;
-
-        const meta = await verifyIfMetaJsonExists(Deno.cwd());
-
-        if (!meta && !container) {
-          console.log('No meta.json found');
-          Deno.exit(0);
-        }
-
-        const name = container || meta?.name;
-
-        await $`docker exec -it ${name} /bin/zsh -c "cd /workspaces/${name} && export PROMPT_EOL_MARK='' && zsh"`;
-      } catch (e) {
-        console.log("Are you sure in the the path of the project's root?");
-        Deno.exit(0);
-      }
-    });
-
-  ////////////////////////////////////////////////////////////////////////////
-  // GENERATE A UUID
-  ////////////////////////////////////////////////////////////////////////////
-
-  misc
     .command('session')
     .description('reset the tasks related to terminal session in tasks.json')
     .action(async () => {
